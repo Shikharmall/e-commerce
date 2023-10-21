6e0fa457adae144db25f4fc850d67f21b9c7dbd6
shikhardatadays <shikhar@datadays.in>
2023-10-21 21:06:05 +0530

    changecartqty

--- src/cartlogic/CartLogic.tsx
+++ src/cartlogic/CartLogic.tsx
@@ -1,44 +1,54 @@
   
   //add item to cart
 
-  export const addItemInCart = (id: number, title: string, amount: number, price: number, image: string)=>{
+  export const addItemInCart = (idd: number, title: string, amountt: number, price: number, image: string)=>{
 
-    let cartItem = {id:id, title:title, amount:amount, price:price, image:image};
+    let cartItem = {id:idd, title:title, amount:amountt, price:price, image:image};
 
-    let totalll = amount*price;
+    try{
 
-    try {
-        const storedValuecarttt = localStorage.getItem('carttt');
+      const storedValuecarttt = localStorage.getItem('carttt');
     
-        if (storedValuecarttt) {
-          const storedValuecarttt1 = JSON.parse(storedValuecarttt);
+      if (storedValuecarttt) {
 
-          /*let totalll1String = localStorage.getItem('totalll'); 
-          let totalll1 = Number(totalll1String); 
-          
-          if (!isNaN(totalll1)) { 
+        const storedValuecarttt1 = JSON.parse(storedValuecarttt);
 
-            totalll1 = totalll1 + totalll; 
-            localStorage.setItem('totalll', totalll1.toString()); 
+        if(Array.isArray(storedValuecarttt1)){
 
-          } */
+          let flag: number = 0;
 
-    
-          if (Array.isArray(storedValuecarttt1)) {
+          //if item already present
+
+          storedValuecarttt1.map((item,index)=>{
+
+            if(item.id === idd){
+
+              flag = 1;
+              storedValuecarttt1[index].amount =  storedValuecarttt1[index].amount + amountt;
+              localStorage.setItem('carttt', JSON.stringify(storedValuecarttt1));
+
+            }
+
+          })
+
+          if(flag === 0){
+            
             const mergedcarttt1 = [...storedValuecarttt1, cartItem];
             localStorage.setItem('carttt', JSON.stringify(mergedcarttt1));
-            localStorage.setItem('carttt', JSON.stringify(mergedcarttt1));
-          } else {
-            console.error('The stored cart is not an array.');
+
           }
 
         } else {
-          localStorage.setItem('carttt', JSON.stringify([cartItem]));
+          console.error('The stored cart is not an array.');
         }
 
-      } catch (error) {
-        console.error('Error parsing or updating the cart:', error);
+      }else{
+        localStorage.setItem('carttt', JSON.stringify([cartItem]));
       }
+
+    }catch(error){
+      console.error('Error parsing or updating the cart:', error);
+    }
 
   }
 
