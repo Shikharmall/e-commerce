3c8479267cdde7d4e57a088921232f62153abc75
shikhardatadays <shikhar@datadays.in>
2023-10-21 21:44:06 +0530

    changecartqty

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,17 +1,10 @@
 import React,{ useState ,useEffect} from 'react';
-//import { useFilterContext } from '../context/filter_context'
-//import { useProductsContext } from '../context/products_context'
 import GridView from './GridView'
-//import ListView from './ListView'
-import Loading from './Loading'
 
 import {getAllProducts} from '../api/AllProducts';
 import { itemDataType } from '../utils/itemData';
 
 const ProductList = () => {
-
-  //const { filteredProducts, gridView } = useFilterContext();
-  //const { productsLoading } = useProductsContext();
 
   const [allProducts,setAllProducts] = useState<itemDataType[]>([]);
 
@@ -30,36 +23,11 @@ const ProductList = () => {
     getallproducts();
   },[]);
 
-  //const prod = [allProducts];
-  //console.log(prod);
-
-  //if (productsLoading) {
-  //  return <Loading />
-  //}
-
-  //if (allProducts.length > 0) {
-  //  return <ListView filteredProducts={allProducts} />;
-  //}
-  //else{
-  //  return (
-  //    <h5 style={{ textTransform: 'none' }}>
-  //     {/* Sorry, no product matches your search...*/}
-  //     No product found
-  //    </h5>
-  //  )
-  //}
-
   if(allProducts.length > 0){
-    //return <ListView filteredProducts={allProducts} />;
     return <GridView filteredProducts={allProducts}/>
   } else {
     return <h1>Loading...</h1>;
   }
-
-  //if (!gridView) {
-  //  return <ListView filteredProducts={filteredProducts} />
-  //}
-  //return <GridView filteredProducts={filteredProducts}>product list</GridView>
 }
 
 export default ProductList
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,6 +1,5 @@
 import React,{useState,useEffect} from 'react'
 import styled from 'styled-components'
-import { useCartContext } from '../context/cart_context'
 import { Link } from 'react-router-dom'
 import { PageHero, CartContent } from '../components'
 
