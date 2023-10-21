09d9f4e86d5c3694509211de6755d8c0f978fae0
shikhardatadays <shikhar@datadays.in>
2023-10-22 03:01:30 +0530

    updatedreadme

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,6 +1,5 @@
 import React, { useState,useEffect } from 'react'
 import styled from 'styled-components'
-import { FiltersButton } from './FiltersButton'
 import { itemDataType } from '../../utils/itemData'
 import { getAllCategory } from '../../api/AllProducts'
 
@@ -29,10 +28,6 @@ const Filters: React.FC<{ searchbynamefunc1: (value: string) => Promise<void>,se
 
   return (
     <Wrapper>
-      <FiltersButton
-        showFilters={showFilters}
-        setShowFilters={setShowFilters}
-      />
 
           <div>
             <form onSubmit={e => e.preventDefault()}>
