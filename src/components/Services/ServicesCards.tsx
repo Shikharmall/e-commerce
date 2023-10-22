3418441209b85fbd573f4718f78c74c55607cd91
shikhardatadays <shikhar@datadays.in>
2023-10-22 14:21:00 +0530

    minor home page change

--- src/components/Services/ServicesCards.tsx
+++ src/components/Services/ServicesCards.tsx
@@ -1,32 +1,12 @@
 import { Link } from 'react-router-dom'
-import { useFilterContext } from '../../context/filter_context'
-import { services } from '../../utils/constants'
 
 import { GiClothes } from 'react-icons/gi'
 import { MdLaptop} from 'react-icons/md';
-import { FaGem, FaRing } from 'react-icons/fa';
+import { FaGem} from 'react-icons/fa';
 
 export const ServicesCards = () => {
-  const { updateFilters, handleClickFromServices, clearFilters } =useFilterContext()
   return (
     <div className='services-center'>
-      {/*{services.map(({ id, icon, title }) => {
-        return (
-          <article key={id} className='service'>
-            <span className='icon'>{icon}</span>
-            <h4>{title}</h4>
-            <Link to='/products'>
-              <button
-                className='btn'
-                type='button'
-                name='home-page-category'
-              >
-                click here for {title}
-              </button>
-            </Link>
-          </article>
-        )
-      })}*/}
           <article  className='service'>
             <span className='icon'><GiClothes /></span>
             <h4></h4>
