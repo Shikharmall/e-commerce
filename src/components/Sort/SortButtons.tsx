7670c2a7a79de4ff09b6aadeb17b9b25361ff56c
shikhardatadays <shikhar@datadays.in>
2023-10-22 13:55:58 +0530

    minor product page change

--- src/components/Sort/SortButtons.tsx
+++ src/components/Sort/SortButtons.tsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import { useFilterContext } from '../../context/filter_context'
+import React, { useState } from 'react'
 import { BsFillGridFill, BsList } from 'react-icons/bs'
 
 export const SortButtons = () => {
-  const { gridView, setGridView, setListView } = useFilterContext()
+  const [ gridView, setGridView ] = useState(true);
+
   return (
     <>
     <div className='btn-container'>
       <button
         type='button'
         className={gridView ? 'active' : undefined}
-        onClick={setGridView}
       >
         <BsFillGridFill />
       </button>
       <button
         type='button'
         className={!gridView ? 'active' : undefined}
-        onClick={setListView}
       >
         <BsList />
       </button>
